6eedc492078bed6c3bc85c2d8fe966de66d8d826
jpark <tmdeksdl@gmail.com>
2022-06-20 11:51:18 +0900

    control 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import {useEffect, useState} from 'react'
-import {Link, Route, Routes, useParams} from 'react-router-dom'
+import {Link, Route, Routes,useNavigate, useParams} from 'react-router-dom'
 function Header() {
   return <header><h1><Link to='/'>WEB</Link></h1></header>
 }
@@ -27,6 +27,44 @@ function Read() {
   return <article><h2>{topic.title}</h2>{topic.body}</article>
 }
 
+function Create() {
+  const nagivate = useNavigate()
+  const submitHandler = async (evt) => {
+    evt.preventDefault();
+    const title = evt.target.title.value;
+    const body = evt.target.body.value;
+
+    const res = await fetch('http://localhost:3333/topics', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({title, body})
+    })
+    const result = await res.json()
+    nagivate('/read/' + result.id)
+
+  }
+
+  return <form onSubmit={submitHandler}>
+    <h2>Create</h2>
+    <p><input type="text" name="title" placeholder='title'></input></p>
+    <p><textarea name='body' placeholder='body'></textarea></p>
+    <p><input type="submit" value="create"></input></p>
+  </form>
+}
+
+function Control(){
+  const param = useParams();
+  const id = Number(param.id)
+  let contextUI = null;
+  if (id) {
+    contextUI = <><Link to={'/update/' + id}>Update</Link></>
+  }
+  return <ul>
+      <li><Link to="/create">Create</Link></li>
+    </ul>
+}
 function App() {
   const [topics, setTopics] = useState([])
   const refreshTopics = async () => {
@@ -49,7 +87,15 @@ function App() {
         
         </Route>
         <Route path='/read/:id' element={<Read></Read>}></Route>
+        <Route path='/create' element={<Create></Create>}></Route>
       </Routes>
+      <Routes>
+        {['/', '/read/:id', '/create'].map(e=>{
+          return <Route key={e} path={e} element={<Control></Control>}></Route>
+        })}
+      </Routes>
+
+      <Control></Control>
     </div>
 
   );
