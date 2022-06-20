5d712a3d47bbde69127f6c93eb6bf093db29f5ec
jpark <tmdeksdl@gmail.com>
2022-06-20 13:36:59 +0900

    context 연습 준비

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 // import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import App from './Context'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
